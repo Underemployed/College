9384ca3ef825d6a5883c20010fb30ae674972110
Underemployed <underemployed871@gmail.com>
2023-07-09 18:55:26 +0530

    Two Sum LeetCode Problem 1

---
+++ Two_Sum_1.js
@@ -0,0 +1,39 @@
+
+// brute force no memory but time complexity O(n^2)
+var twoSum=function(nums,target){
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length-1;j++){
+            if(target==nums[i]+nums[j] && i!=j)
+            return [i,j];
+        }
+    }
+};
+
+// scrapped easier to do mapping
+// sort nlog(n)  time: o(nlogn) space:o(n) size of array
+// var twoSum=function(nums,target){
+//     let copy=[...nums];
+//     copy.sort((a,b)=>a-b);//sort doesnt return value
+//     for(let i=0;i<copy.length-1;i++){
+//         for(let j=copy.length-1;j>i;j--){
+//             if(target===copy[i]+copy[j]){
+//                 index1=nums.indexOf(copy[i]);// starting value for searching
+//                 //indexOf(searchValue,parameters)
+//                 return [nums.indexOf(copy[i]),nums.indexOf(copy[j],index1+1)]
+//             }
+//         }
+//     }
+// };
+
+var twoSum=function(nums,target){
+    hash={}
+    for(let i=0;i<nums.length;i++){
+        hash[nums[i]]=i;
+    }
+    for(let i=0;i<nums.length;i++){
+        let potkey=target-nums[i]
+        if(hash[potkey] && hash[potkey] !==i){
+            return [hash[potkey],i]
+        }
+    }
+};
