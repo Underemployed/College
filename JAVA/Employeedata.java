86bce23326f24ce2fb3c5647da5dd375ae0d78ae
Underemployed <underemployed871@gmail.com>
2024-03-14 21:54:21 +0530

    arraylist object employee database

--- JAVA/Employeedata.java
+++ JAVA/Employeedata.java
@@ -4,30 +4,67 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Employeedata {
-    static List<Employee> employees = new ArrayList<>();
 
     public static void main(String[] args) {
-        employees.add(new Employee("wow", 1000));
-        employees.add(new Employee("nithin", 11000));
-        new Employee("how", 1000).sum(employees);
+        Employee a = new Employee("wow", 1000);
+
+        new Employee("nithin", 11000);
+        Employee.sum();
     }
 }
 
+/* using ArrayList */
+// class Employee {
+
+// String name;
+// int salary;
+// static List<Employee> employees = new ArrayList<>();
+
+// public Employee(String name, int salary) {
+// this.name = name;
+// this.salary = salary;
+// employees.add(this);
+// }
+
+// static void sum() {
+// int s = 0;
+// for (Employee a : employees) {
+// s = s + a.salary;
+// }
+// System.out.println("The total salary is: " + s);
+// }
+// }
+
+/* Using Array Objects */
 class Employee {
 
     String name;
     int salary;
+    static Employee[] employees = new Employee[10];
+    static int i = 0;
 
     public Employee(String name, int salary) {
         this.name = name;
         this.salary = salary;
+        if (i < employees.length) {
+            employees[i++] = this;
+        } else {
+            System.out.println("No more space in the array");
+        }
     }
 
-    void sum(List<Employee> employees) {
+    static void sum() {
         int s = 0;
         for (Employee a : employees) {
-            s = s + a.salary;
+            if (a != null) {
+
+                s = s + a.salary;
+            }
         }
         System.out.println("The total salary is: " + s);
+    }
+
+    void rrr(Employee a) {
+
     }
 }
