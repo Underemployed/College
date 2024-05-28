30e68615f6b76fbda43bcda064d4d2df0af060d6
Underemployed <underemployed871@gmail.com>
2024-05-28 20:55:28 +0530

    threading how to create thread

--- JAVA/multithreadthing.java
+++ JAVA/multithreadthing.java
@@ -15,10 +15,47 @@ class myThread extends Thread{
     }
 }
 
+class myRunnable implements Runnable{
+
+    public void run() {        
+        for(int i =1 ;i<100;i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {}
+        }
+    }
+        
+}
+
+
+
+
 public class multithreadthing {
 
      public static void main(String[] args) {
-        
+        // extend thread
+        myThread extthreadobj = new myThread();
         System.out.println("main");
-     }
+
+        // without class direct
+        Thread threadwithoutclas= new Thread(
+            new Runnable() {
+                public void run(){
+                    for(int i =1 ;i<100;i++){
+                        System.out.println(i);
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        );// new thread closing bracket
+
+        
+        // using runnable interface
+        myRunnable runableobj = new myRunnable();
+        Thread runablethread = new Thread(runableobj);
+    }
 }
--- JAVA/outer.java
+++ JAVA/outer.java
@@ -16,11 +16,16 @@ public class outer{
 
 class outerinnermain{
     public static void main(String[] args) {
-        outer.statinner statinestedobject = new outer.statinner(); // inner  class static
-        // cause static class doesnt depend on the instance of the parent also doenst inherit method and objects
-        outer outerObject = new outer(); // inner normal class
+        outer.statinner statinestedobject = new outer.statinner(); //static inner class 
+   
+        outer outerObject = new outer(); // inner class
         outer.inner innerobject = outerObject.new inner();
-        // non static inner class inherits everything
 
+
+
+        // ***An inner class has access to all of the members of its enclosing class, but
+        // the reverse is not true. Members of the inner class are known only within the scope of the
+        // inner class and may not be used by the outer class
+        // cant acess methods directly from object
     }
 }
