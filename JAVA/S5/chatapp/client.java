b8b6e8f34ca08cad2db25153150a7e01278713eb
Underemployed <underemployed871@gmail.com>
2024-10-02 22:39:45 +0530

    i am stupid

--- JAVA/S5/chatapp/client.java
+++ JAVA/S5/chatapp/client.java
@@ -7,35 +7,52 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
 
 public class client {
+    static Scanner reader = null;static PrintWriter sender = null;
 
     public static void main(String[] args) {
         try {
             System.out.println("client started....");
             Socket server = new Socket("localhost", 9801);
+
+
+            InputStreamReader server_input = new InputStreamReader(server.getInputStream());
+            OutputStreamWriter server_output = new OutputStreamWriter(server.getOutputStream());
+
+            sender =  new PrintWriter(server_output,true);
+            reader = new Scanner(server_input);
+            Scanner sc = new Scanner(System.in);
+
             
-            PrintWriter out = new PrintWriter(server.getOutputStream(), false);
-            BufferedReader in  = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String msgin = "", msgout = "";
+            Thread printer = new Thread(
+                new Runnable() {
+                    public void run(){
+                        String msg = "";
+                        while(true){
+                            if(!(msg = reader.nextLine()).equals("bye")){
+                                    System.out.println("server: "+msg);
+                            }else{
+                                break;
+                            }
 
-            do {
+                        }
+                    }
+                }
+            );
+
+            printer.start();
+
+            String send =null;
+            while ((send =sc.nextLine())!= "bye") {
+                sender.println(send);                
+            }
 
 
-
-            } while (!msgin.equals("bye"));
-
-
-
-
-
-
-
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
+    
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
--- JAVA/S5/chatapp/server.java
+++ JAVA/S5/chatapp/server.java
@@ -1,41 +1,53 @@
 package JAVA.S5.chatapp;
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+// cd JAVA/S5/chatapp
 public class server {
-    
+    static Scanner reader = null;static PrintWriter sender = null;
 
     public static void main(String[] args) {
-        
+
 
         try {
             ServerSocket ss = new ServerSocket(9801);
             Socket client = ss.accept();
+            System.out.println("Client connected");
+
+            InputStreamReader client_input = new InputStreamReader(client.getInputStream());
+            OutputStreamWriter client_output = new OutputStreamWriter(client.getOutputStream());
+            Scanner sc = new Scanner(System.in);
+
+            sender =  new PrintWriter(client_output,true);
+            reader = new Scanner(client_input);
+            
+
+            Thread printer = new Thread(
+                new Runnable() {
+                    public void run(){
+                        String msg = "";
+                        while(true){
+                            if(!(msg = reader.nextLine()).equals("bye")){
+                                System.out.println("client: "+msg);
+                            }else{
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            );
+            printer.start();
 
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()),true);
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String send =null;
+            while ((send =sc.nextLine())!= "bye") {
+                sender.println(send);
+            }
 
 
-            String msgin ="",msgout = "";
-
-            do {
-                while(nums[i+1] == nums[i]) i++;
-                msgin = in.readLine();
-                out.println(msgout);
-                msgout = br.readLine();
-                out.println(msgout);
-
-            } while (!msgin.equals("bye"));
-
+            ss.close();
 
         } catch (IOException e) {
             e.printStackTrace();
